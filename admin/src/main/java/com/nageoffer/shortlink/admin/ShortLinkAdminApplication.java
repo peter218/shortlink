4e9a5ad125518ff104c9c2921f6769c9c8313b5e
吕浩源 <14607544+lv-haoyuan218@user.noreply.gitee.com>
2024-09-09 16:55:24 +0800

    shortlink-module
    短链接项目及子模块

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,17 @@
+package com.nageoffer.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * title:
+ * author:Lv Haoyuan
+ * date:2024-09-09
+ * description:
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class,args);
+    }
+}
