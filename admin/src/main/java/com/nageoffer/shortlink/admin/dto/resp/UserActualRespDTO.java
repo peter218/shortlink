753d22c531b5fe8af64c3bd520015922a61095c9
吕浩源 <14607544+lv-haoyuan218@user.noreply.gitee.com>
2024-09-13 12:51:51 +0800

    脱敏用户信息

--- admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/UserActualRespDTO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/UserActualRespDTO.java
@@ -1,7 +1,5 @@
 package com.nageoffer.shortlink.admin.dto.resp;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.nageoffer.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 /**
@@ -11,7 +9,7 @@ import lombok.Data;
  * description:用户返回参数实体
  */
 @Data
-public class UserRespDTO {
+public class UserActualRespDTO {
     /**
      * id
      */
@@ -30,7 +28,6 @@ public class UserRespDTO {
     /**
      * 手机号
      */
-    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
